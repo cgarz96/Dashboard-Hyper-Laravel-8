5c00a12b973d618f900847dac3837348879e065c
cgarz96 <cgarzon388@gmail.com>
2022-05-21 22:46:04 -0300

    Agregando plantilla

---
+++ resources/views/layouts/admin/page-title.blade.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box">
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                    <li class="breadcrumb-item active">Preloader Example</li>
+                </ol>
+            </div>
+            <h4 class="page-title">Dashboard With Preloader</h4>
+        </div>
+    </div>
+</div> 
